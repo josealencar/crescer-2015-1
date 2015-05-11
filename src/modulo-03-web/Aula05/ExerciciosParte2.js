0202cbae49a0f6f1ef0f71cbc5b2c47f628d4af4
José Alencar de Souza Rocha <jose_alencar_1403@hotmail.com>
2015-05-11 00:06:34 -0300

    feat(ExerciciosParte2.js) Adicionar exercícios parte 2 de JS (rever itens 2B e 2C);

---
+++ src/modulo-03-web/Aula05/ExerciciosParte2.js
@@ -0,0 +1,88 @@
+//Lista de Exercícios JS parte 2
+//Exercício 01
+var brewdog = function(a, b){
+    var array1 = a.split('');
+    var array2 = b.split('');
+    var contador = 0;
+    var len = array1.length < array2.length? array1.length: array2.length;
+    for (var i = 0; i < len; i ++){
+        var teste1 = array1[i];
+        var teste2 = array2[i];
+        if (teste1 === teste2){
+            contador = contador + 1;
+        }
+    }
+    var retorno = array2.length - contador;
+    return retorno;
+};
+
+//Exercício 02
+//Exercício 2A
+function Emprestimo(valorTotal, taxaJuros, quantidadeParcelas){
+    quantidadeParcelas = quantidadeParcelas || 3;
+    
+    this.valorTotal = valorTotal;
+    this.taxaJuros = taxaJuros;
+    this.quantidadeParcelas = quantidadeParcelas;
+};
+
+//Exercício 2B
+function Emprestimo(valorTotal, taxaJuros, quantidadeParcelas){
+    quantidadeParcelas = quantidadeParcelas || 3;
+    
+    this.valorTotal = valorTotal;
+    this.taxaJuros = taxaJuros;
+    this.quantidadeParcelas = quantidadeParcelas;
+    
+    this.valorParcela = function(){
+        var valor = valorTotal / ((1 - (Math.pow((1+taxaJuros), -quantidadeParcelas))) / taxaJuros);
+        return valor.toFixed(2);
+    }
+};
+
+//Exercício 2C
+function Emprestimo(valorTotal, taxaJuros, quantidadeParcelas){
+    quantidadeParcelas = quantidadeParcelas || 3;
+    
+    this.valorTotal = valorTotal;
+    this.taxaJuros = taxaJuros;
+    this.quantidadeParcelas = quantidadeParcelas;
+    
+    this.valorParcela = function(){
+        var valor = valorTotal / ((1 - (Math.pow((1+taxaJuros), -quantidadeParcelas))) / taxaJuros);
+        return valor.toFixed(2);
+    }
+    
+    this.valorTotalJuros = function(){
+        var valor = this.valorParcela() * quantidadeParcelas - valorTotal;
+        return valor.toFixed(2);
+    }
+};
+
+//Exercício 03
+function palindromo(String){
+    var ePalindromo = false;
+    var palavras = ['iluminatti','ledesma','dante','verdemusgo','bacon'];
+    var er = /[@$1234567890©`´~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi;
+    var comparar = String.replace('/\s/gi','');
+    comparar = comparar.replace(er,'');
+	comparar = comparar.replace(/[âáàã]/gi,"a");
+	comparar = comparar.replace(/[éèê]/gi,"e");
+	comparar = comparar.replace(/[íìî]/gi,"i");
+	comparar = comparar.replace(/[ôõóò]/gi,"o");
+	comparar = comparar.replace(/[úùû]/gi,"u");
+	comparar = comparar.replace("ç","c");
+    comparar = comparar.toLowerCase();
+    var len = palavras.length;
+    for(var i=0; i<len; i++){
+        var testar = palavras[i];
+        if(comparar === testar){
+            ePalindromo = true;
+        }
+    }
+    var testar = comparar.split('').reverse().join('');
+    if(comparar === testar){
+        ePalindromo = true;
+    }
+    return ePalindromo;
+};
