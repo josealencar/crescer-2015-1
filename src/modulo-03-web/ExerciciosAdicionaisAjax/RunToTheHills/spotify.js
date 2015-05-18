0fa9c75466ba1b8aaf47da3c1b83030e7e022d53
José Alencar de Souza Rocha <jose_alencar_1403@hotmail.com>
2015-05-18 14:11:11 -0300

    feat(RunToTheHills) Adicionar exercício 01 de Ajax

---
+++ src/modulo-03-web/ExerciciosAdicionaisAjax/RunToTheHills/spotify.js
@@ -0,0 +1,11 @@
+$(function(){ //http://content.guardianapis.com/search?q=manchester%20united&api-key=test
+    $.ajax({url: 'https://api.spotify.com/v1/artists/6mdiAmATAx73kdxrNrnlao/albums?limit=50', type: 'GET'})
+        .done(function(res){resultado(res);});
+});
+
+function resultado(res) {
+    albums = $.extend({}, res.items);
+		$.each(albums, function(i, umAlbum){
+			$('.container').append($('<img/>', {src: umAlbum.images[1].url}));
+		})
+};
