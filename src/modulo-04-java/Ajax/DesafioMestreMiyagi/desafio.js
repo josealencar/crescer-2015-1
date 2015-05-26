89cb9365da78862ecee7b47876e8021fe3e6bee6
José Alencar de Souza Rocha <jose_alencar_1403@hotmail.com>
2015-05-26 13:53:58 -0300

    feat(DesafioMestreMiyagi) Adicionar exercício finalizado de Ajax

--- src/modulo-04-java/Ajax/DesafioMestreMiyagi/desafio.js
+++ src/modulo-04-java/Ajax/DesafioMestreMiyagi/desafio.js
@@ -1,6 +1,6 @@
 var nome;
 $.ajax({
-    url: 'http://karatekid.instaweb.com.br:4567/apresentar?nome=José',
+    url: 'http://karatekid.instaweb.com.br:4567/apresentar?nome=Jose',
     type: 'POST'/*,
     contentType: 'application/jason',
     dataType: 'json',
@@ -16,33 +16,41 @@ $.ajax({
         contentType: 'application/jason',
         dataType: 'json',
         data: JSON.stringify(dados)*/
-    }).done(function(resposta){
-        console.log(resposta);
     }).done(function(res){ exercicios(res); console.log(res.atividades);});
 });
 
 function exercicios(res){
     $.ajax({
-        url: 'http://karatekid.instaweb.com.br:4567/tarefas?nome='+nome+res.atividades[0].atividade,
-        type: 'POST',
+        url: 'http://karatekid.instaweb.com.br:4567'+res.atividades[0].atividade+'?nome='+nome,
+        type: 'POST'/*,
         contentType: 'application/jason',
-        dataType: 'json'/*,
+        dataType: 'json',
         data: JSON.stringify(dados)*/
     }).done(function(res){console.log('0', res)});
     
     $.ajax({
-        url: 'http://karatekid.instaweb.com.br:4567/tarefas?nome='+nome+res.atividades[1].atividade,
-        type: 'POST',
+        url: 'http://karatekid.instaweb.com.br:4567'+res.atividades[1].atividade+'?nome='+nome,
+        type: 'POST'/*,
         contentType: 'application/jason',
-        dataType: 'json'/*,
+        dataType: 'json',
         data: JSON.stringify(dados)*/
     }).done(function(res){console.log('1', res)});
     
     $.ajax({
-        url: 'http://karatekid.instaweb.com.br:4567/tarefas?nome='+nome+res.atividades[2].atividade,
-        type: 'POST',
+        url: 'http://karatekid.instaweb.com.br:4567'+res.atividades[2].atividade+'?nome='+nome,
+        type: 'POST'/*,
         contentType: 'application/jason',
-        dataType: 'json'/*,
+        dataType: 'json',
         data: JSON.stringify(dados)*/
-    }).done(function(res){console.log('2', res)});
+    }).done(function(res){console.log('2', res); acabei();});
+};
+
+function acabei(){
+    $.ajax({
+        url: 'http://karatekid.instaweb.com.br:4567/acabei?nome='+nome,
+        type: 'POST'/*,
+        contentType: 'application/jason',
+        dataType: 'json',
+        data: JSON.stringify(dados)*/
+    })
 };
