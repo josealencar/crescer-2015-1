87af56a9e1f0c346ee78c69d72c7cf9e50d5a6aa
José Alencar de Souza Rocha <jose_alencar_1403@hotmail.com>
2015-06-01 13:53:18 -0300

    feat(DesafioSQL) Adicionar início do desafio - Falta concluir

--- src/modulo-02-sql/Oracle/CriacaoDeUsuarioEPermissoes.sql
+++ src/modulo-02-sql/Oracle/CriacaoDeUsuarioEPermissoes.sql
@@ -12,3 +12,18 @@ create user crescer15 identified by crescer15;
 
 -- Concedendo permissões para conectar e criar objetos
 grant connect, resource, create view to crescer15;
+
+Create tablespace DESAFIO1_DAT
+datafile 'C:\oraclexe\app\oracle\oradata\XE\desafio1_dat01.dbf'
+size 100m
+autoextend on
+next 100m
+maxsize 6144m;
+
+Create user DESAFIO1 identified by DESAFIO1 default tablespace DESAFIO1_DAT;
+
+Grant connect, resource, create view to DESAFIO1;
+
+Select directory_path
+from dba_directories
+where directory_name = 'DATA_PUMP_DIR';
